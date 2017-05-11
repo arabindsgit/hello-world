9143114fe0482c309104526683c632cea52ba53e
arabindsgit <arabind.arabind@wipro.com>
2017-05-11 10:42:01 +0530

    hello world program cs

--- test.cs
+++ test.cs
@@ -1,1 +1,15 @@
+import System;
+import System.Collections.Generic;
+import System.Linq;
+import System.Text;
 
+namespace Test
+{
+  public class Program
+  {
+    static void main()
+    {
+      Console.WriteLine("Hello world!");
+    }
+  }
+}
